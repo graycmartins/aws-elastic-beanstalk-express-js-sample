562cd526b82c2a99aa8e1e667bb218203ec9b7fa
graycmartins <gray.martins@gmail.com>
2023-01-26 15:18:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-<<<<<<< HEAD
-app.get('/', (req, res) => res.send('Hello World224!'));
-=======
 app.get('/', (req, res) => res.send('Hello World!'));
->>>>>>> parent of 21e91a1 (Update app.js)
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
